9ac015a1e2b2cedcac85ec101464f185e9a6db1a
Georgi Georgiev <Georgi Georgiev@DESKTOP-9FI4TKH>
2025-02-10 12:20:14 +0200

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,33 @@
+﻿// if - else
+//Console.WriteLine("Type the numbers!");
+//int a = int.Parse(Console.ReadLine());
+//int b = int.Parse(Console.ReadLine());
+//int c = int.Parse(Console.ReadLine());
+
+//if (a > 0)
+using System.ComponentModel;
+
+{
+    //if (b > 0)
+   // { c = 20; }
+   // else
+   // { c = -20; }
+
+}
+//Console.WriteLine(c);
+
+
+
+Console.WriteLine("Type the numbers");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+if (a > 0)
+{
+    if (b>0)
+        c = 0;
+}
+else c = 3;
+Console.WriteLine(c);
+    
